3b5042533d7fd88d91728ee3a0ce85657af0cc91
[Emily Echarri] <[e@emilyecharri.com]>
2021-09-08 09:39:28 -0700

    changed the variable from fry to fryQuote etc. and renamed personQuote and made the variable constant across addQuoteToMessage and getRandomQuoteFromMessage

--- mixed-messages futurma/script.js
+++ mixed-messages futurma/script.js
@@ -66,12 +66,12 @@ getRandomQuotefromMessage(personQuote){
     return message[randomQuote];
 },
 generateRandomQuote() {
-    const fryQuotes = this.getRandomQuotefromMessage('fry');
-    const benderQuotes = this.getRandomQuotefromMessage('bender');
-    const leelaQuotes = this.getRandomQuotefromMessage('leela');
-    const drZoidbergQuotes = this.getRandomQuotefromMessage('dr zoidberb');
-    const amyQuotes = this.getRandomQuotefromMessage('amy');
-    const professorQuotes = this.getRandomQuotefromMessage('professor');
+    const fryQuotes = this.getRandomQuotefromMessage('fryQuotes');
+    const benderQuotes = this.getRandomQuotefromMessage('benderQuotes');
+    const leelaQuotes = this.getRandomQuotefromMessage('leelaQuotes');
+    const drZoidbergQuotes = this.getRandomQuotefromMessage('drZoidbergQuotes');
+    const amyQuotes = this.getRandomQuotefromMessage('amyQuotes');
+    const professorQuotes = this.getRandomQuotefromMessage('professorQuotes');
 
 
 
@@ -80,18 +80,19 @@ generateRandomQuote() {
 
 };
 
-mixedMessagesFuturama.addQuoteToMessage('fry', 'Valentine’s Day is coming? Oh crap – I forgot to get a girlfriend again.', 'Fry');
-mixedMessagesFuturama.addQuoteToMessage('dr zoidberg', 'Your music’s bad and you should feel bad!', 'Dr. Zoidberg');
-mixedMessagesFuturama.addQuoteToMessage('bender','I’m so embarrassed. I wish everybody else was dead.', 'Bender');
-mixedMessagesFuturama.addQuoteToMessage('leela', 'This is my first visit to the Galaxy of Terror and I’d like it to be a pleasant one.', 'Leela');
-mixedMessagesFuturama.addQuoteToMessage('professor', 'Good news, everyone! I’ve taught the toaster to feel love!', 'Professor');
-mixedMessagesFuturama.addQuoteToMessage('bender', 'Game’s over, losers! I have all the money. Compare your lives to mine and then kill yourselves.', 'Bender');
-mixedMessagesFuturama.addQuoteToMessage('dr zoidberg','Two oil changes for the price of one! Now if I could afford the one, and the car.','Dr. Zoidberg');
-mixedMessagesFuturama.addQuoteToMessage('fry', 'My folks were always on me to groom myself and wear underpants. What am I, the pope?', 'Fry');
-mixedMessagesFuturama.addQuoteToMessage('leela','Fry, please try to understand. You’re a man. I’m a woman. We’re just too different.', 'Leela');
-mixedMessagesFuturama.addQuoteToMessage('amy', 'Finally, a uniform I’d be happy to be caught dead in!', 'Amy');
-mixedMessagesFuturama.addQuoteToMessage('bender', 'Hey sexy mama. Wanna kill all humans?', 'Bender');
-
+mixedMessagesFuturama.addQuoteToMessage('fryQuotes', 'Valentine’s Day is coming? Oh crap – I forgot to get a girlfriend again.', 'Fry');
+mixedMessagesFuturama.addQuoteToMessage('drZoidbergQuotes', 'Your music’s bad and you should feel bad!', 'Dr. Zoidberg');
+mixedMessagesFuturama.addQuoteToMessage('benderQuotes','I’m so embarrassed. I wish everybody else was dead.', 'Bender');
+mixedMessagesFuturama.addQuoteToMessage('leelaQuotes', 'This is my first visit to the Galaxy of Terror and I’d like it to be a pleasant one.', 'Leela');
+mixedMessagesFuturama.addQuoteToMessage('professorQuotes', 'Good news, everyone! I’ve taught the toaster to feel love!', 'Professor');
+mixedMessagesFuturama.addQuoteToMessage('benderQuotes', 'Game’s over, losers! I have all the money. Compare your lives to mine and then kill yourselves.', 'Bender');
+mixedMessagesFuturama.addQuoteToMessage('drZoidbergQuotes','Two oil changes for the price of one! Now if I could afford the one, and the car.','Dr. Zoidberg');
+mixedMessagesFuturama.addQuoteToMessage('fryQuotes', 'My folks were always on me to groom myself and wear underpants. What am I, the pope?', 'Fry');
+mixedMessagesFuturama.addQuoteToMessage('leelaQuotes','Fry, please try to understand. You’re a man. I’m a woman. We’re just too different.', 'Leela');
+mixedMessagesFuturama.addQuoteToMessage('amyQuotes', 'Finally, a uniform I’d be happy to be caught dead in!', 'Amy');
+mixedMessagesFuturama.addQuoteToMessage('benderQuotes', 'Hey sexy mama. Wanna kill all humans?', 'Bender');
+mixedMessagesFuturama.addQuoteToMessage('professorQuotes',"Everyone's always in favor of saving Hitler's brain, but when you put it in the body of a great white shark. Ohhh, suddenly you've gone too far.", 'professor');
+mixedMessagesFuturama.addQuoteToMessage('amyQuotes', "Hey, let's go car shopping! My parents promised if I got all B's they'd buy me a bar, and I got all C's!", 'Amy');
 
 
 
